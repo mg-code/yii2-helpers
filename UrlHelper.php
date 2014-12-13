4cdd1fdec64bd8a2c5d6fc0ca8fc36954823407c
Maris Graudins <maris.graudins91@gmail.com>
2014-12-13 20:28:58 +0200

    UrlHelper

---
+++ UrlHelper.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace mgcode\helpers;
+
+class UrlHelper extends \yii\helpers\Url
+{
+    /**
+     * Generate SLUG string max 120 characters long.
+     * Only ASCII characters are allowed.
+     * Uses old but tested library
+     * @param string $string
+     * @param string mixed $separator
+     * @return string
+     */
+    public static function toSlug($string, $separator = '-')
+    {
+        return \mgcode\helpers\vendors\UrlTransliterate::cleanString($string, $separator);
+    }
+
+    /**
+     * Add's parameters to url string.
+     * Overrides existing parameters with new ones.
+     * @param string $url
+     * @param array $params
+     * @return string
+     */
+    public static function addParamsToUrl($url, $params)
+    {
+        // Parse url
+        $parseUrl = parse_url($url);
+
+        // Parse existing parameters
+        $query = [];
+        if (isset($parseUrl['query'])) {
+            $query = static::parseUrlQuery($parseUrl['query']);
+        }
+
+        // Append new queries and override existing ones
+        $query = array_merge($query, $params);
+
+        // Build new URL
+        $cleanUrl = isset($parseUrl['scheme']) ? $parseUrl['scheme'].'://' : '';
+        $cleanUrl .= isset($parseUrl['host']) ? $parseUrl['host'] : '';
+        $cleanUrl .= isset($parseUrl['port']) ? ':'.$parseUrl['port'] : null;
+        $cleanUrl .= isset($parseUrl['path']) ? $parseUrl['path'] : null;
+        $cleanUrl .= $query ? '?'.http_build_query($query) : null;
+        $cleanUrl .= isset($parseUrl['fragment']) ? '#'.$parseUrl['fragment'] : null;
+
+        return $cleanUrl;
+    }
+
+    /**
+     * Parses url query to parameters
+     * @param $query
+     * @return array
+     */
+    public static function parseUrlQuery($query)
+    {
+        $data = preg_replace_callback('/(?:^|(?<=&))[^=[]+/', function ($match) {
+            return bin2hex(urldecode($match[0]));
+        }, $query);
+
+        parse_str($data, $values);
+
+        return array_combine(array_map('hex2bin', array_keys($values)), $values);
+    }
+}
