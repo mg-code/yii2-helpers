88a431a3ffd4e21700501a51176c4721535b9afc
Maris Graudins <maris.graudins91@gmail.com>
2015-08-25 19:56:53 +0300

    Added random unique number generator.

--- NumberHelper.php
+++ NumberHelper.php
@@ -13,4 +13,25 @@ class NumberHelper
     {
         return str_pad($value, $length, '0', STR_PAD_LEFT);
     }
+
+    /**
+     * Returns random unique integer ID as string
+     * Datatype for database is BIGINT(20)
+     * Description:
+     * unix timestamp without first digit
+     * plus
+     * 1/1000 of second (always 4 digits)
+     * plus
+     * random digit in range from 100000 to 999999
+     * @return string
+     */
+    public static function getGuid()
+    {
+        list($usec, $sec) = explode(' ', microtime());
+        $sec = substr($sec, 1, 9);
+        $usec = substr($usec, 2, 4);
+        $guid = $sec.$usec.mt_rand(100000, 999999);
+
+        return $guid;
+    }
 }
