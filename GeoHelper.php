e8767b5cef9f1d83eb0d3d0195b5af51953fbd29
Maris Graudins <maris@mg-interactive.lv>
2017-01-09 13:18:34 +0200

    Enh.: Added `GeoHelper.getDistance` function.

--- GeoHelper.php
+++ GeoHelper.php
@@ -19,4 +19,36 @@ class GeoHelper
         $km = round($meters / 1000, $kmPrecision);
         return $km.'km';
     }
+
+    /**
+     * Returns distance between two points. By default this function returns result in meters.
+     * Function taken from: http://stackoverflow.com/questions/10053358/measuring-the-distance-between-two-coordinates-in-php
+     * @param float $fromLat
+     * @param float $fromLng
+     * @param float $toLat
+     * @param float $toLng
+     * @param bool $returnMiles
+     * @return float
+     */
+    public static function getDistance($fromLat, $fromLng, $toLat, $toLng, $returnMiles = false)
+    {
+        $earthRadius = $returnMiles ? 3959 : 6371000;
+
+        // convert from degrees to radians
+        $latFrom = deg2rad($fromLat);
+        $lonFrom = deg2rad($fromLng);
+        $latTo = deg2rad($toLat);
+        $lonTo = deg2rad($toLng);
+
+        $lonDelta = $lonTo - $lonFrom;
+        $a = pow(cos($latTo) * sin($lonDelta), 2) + pow(cos($latFrom) * sin($latTo) - sin($latFrom) * cos($latTo) * cos($lonDelta), 2);
+        $b = sin($latFrom) * sin($latTo) + cos($latFrom) * cos($latTo) * cos($lonDelta);
+
+        $angle = atan2(sqrt($a), $b);
+        $result = $angle * $earthRadius;
+        if(!$returnMiles) {
+            $result = round($result);
+        }
+        return (float) $result;
+    }
 }
