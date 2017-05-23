c23da0c92460d6c512a3a90510104605b6009b5b
Maris Graudins <maris@mg-interactive.lv>
2017-05-23 11:47:50 +0300

    Enh.: Added JS helper for array shuffling.

--- assets/mgcode.helpers.js
+++ assets/mgcode.helpers.js
@@ -223,10 +223,28 @@ mgcode.helpers = (function ($) {
         }
     };
 
+    var arrayHelper = {
+        /**
+         * Shuffles array in place.
+         * Taken from: http://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+         * @param {Array} a items The array containing the items.
+         */
+        shuffle: function(a) {
+            var j, x, i;
+            for (i = a.length; i; i--) {
+                j = Math.floor(Math.random() * i);
+                x = a[i - 1];
+                a[i - 1] = a[j];
+                a[j] = x;
+            }
+        }
+    };
+
     return {
         number: numberHelper,
         request: requestHelper,
         url: urlHelper,
-        time: timeHelper
+        time: timeHelper,
+        array: arrayHelper
     };
 })(jQuery);
